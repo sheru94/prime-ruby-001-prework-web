84be47bf299dc7a971d94aafcba0ff23724bef34
sheru94 <singhs94@hotmail.com>
2016-04-17 19:01:51 +0000

    nitrous-backup at Sun Apr 17 19:01:51 UTC 2016

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+
+def prime?(integer)
+  d = (1..100).to_a
+  d.each do|num|
+    integer/num
+  if integer%num == 0 || integer%num == 1
+    false
+    else
+    true
+  end
+end
+end# Add  code here!
